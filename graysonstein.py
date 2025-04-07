71d9a3afb1e877f16d05152005bd682e14ce3e8a
GraysonStein <graysonstein53@gmail.com>
2025-04-06 17:21:07 -0700

    Add files via upload

---
+++ graysonstein.py
@@ -0,0 +1,11 @@
+#Function 1
+#Rect_area
+# Returns Area of Rectangle
+
+# Request the dimension of a rectangle
+length = int(input("Enter the length of the the object as a integer: "))
+width = int(input("Enter the width of the the object as a integer: "))
+rect_area = (length * width)
+#Answer
+print("Length = ", length, "Width = ", width)
+print("Area of the rectangle: " + str(rect_area))
